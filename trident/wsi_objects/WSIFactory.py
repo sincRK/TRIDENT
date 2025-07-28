240a66321c491a1bb3a4911c20e82ea1295be4bc
sinclair <sinclair@pharmazie.uni-freiburg.de>
2025-07-28 14:42:15 +0200

    ISytnaxWSI class for integrating isyntax files in the trident workflow

--- trident/wsi_objects/WSIFactory.py
+++ trident/wsi_objects/WSIFactory.py
@@ -79,8 +79,12 @@ def load_wsi(
 
     elif reader_type is None:
         if ext in ISYNTAX_EXTENSIONS:
+<<<<<<< HEAD
             max_workers = kwargs.pop("max_workers", 1)
             return ISyntaxWSI(slide_path=slide_path, max_workers=max_workers, **kwargs)
+=======
+            return ISyntaxWSI(slide_path=slide_path, **kwargs)
+>>>>>>> ISytnaxWSI class for integrating isyntax files in the trident workflow
         elif ext in OPENSLIDE_EXTENSIONS:
             return OpenSlideWSI(slide_path=slide_path, **kwargs)
         else:
